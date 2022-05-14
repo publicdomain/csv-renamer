4b0ff7669be9bb0c14fceed802cfc27bcb886b1a
Victor <publicdomainvic@gmail.com>
2022-05-14 12:03:06 -0400

    Add OnItemsListViewDragEnter()

--- CSVrenamer/MainForm.cs
+++ CSVrenamer/MainForm.cs
@@ -54,7 +54,7 @@ namespace CSVrenamer
         /// <param name="e">Event arguments.</param>
         private void OnItemsListViewDragEnter(object sender, DragEventArgs e)
         {
-            // TODO Add code
+            e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
         }
 
         /// <summary>
