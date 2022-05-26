a9b87b21976b0e8e992ba9833e096f9ca1480380
Victor <publicdomainvic@gmail.com>
2022-05-25 20:48:23 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick()

--- CSVrenamer/MainForm.cs
+++ CSVrenamer/MainForm.cs
@@ -259,7 +259,8 @@ namespace CSVrenamer
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub repository
+            Process.Start("https://github.com/publicdomain/csv-renamer");
         }
 
         /// <summary>
